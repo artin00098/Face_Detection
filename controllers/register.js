09ee87c4ddb3d252e15d4522d7d49e28a756ae9c
artin <artinzabani@gmail.com>
2020-05-31 22:58:29 +0430

    lallal

--- controllers/register.js
+++ controllers/register.js
@@ -3,7 +3,7 @@ const handleRegister = (req,res,db,bcrypt)=>{
 		email:email,
 		password : password
 	}));
-	// const {email , name , password} = req.body;
+	const {email , name , password} = req.body;
 	// if (!email || !name || !password ) {
 	// 	return res.status(400).json('bad registration')
 	// }
